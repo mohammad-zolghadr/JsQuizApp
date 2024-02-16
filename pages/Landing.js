52a3d0a2ade5a94082bbe6e46f1c33067be0949f
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2024-02-16 18:34:59 +0330

    Update: landing Page - 03
    Button Click Handle

--- pages/Landing.js
+++ pages/Landing.js
@@ -1,11 +1,18 @@
 const Landing = () => {
+  const selectDifficulty = (e) => {
+    console.log(e.target.name);
+  };
+
   return `
   <div class="landingContainer">
   <h2>به بازی QuizApp خوش اومدی </h2>
   <h4>برای شروع، میزان سختی مدنظرت رو انتخاب کن تا بازی شروع شه</h4>
-    <button>ساده</button>
-    <button>متوسط</button>
-    <button>سخت</button>
+    <button name='easy' onclick='${(onclick = (e) =>
+      selectDifficulty(e))}'>ساده</button>
+    <button name='medium'  onclick='${(onclick = (e) =>
+      selectDifficulty(e))}'>متوسط</button>
+    <button name='hard'  onclick='${(onclick = (e) =>
+      selectDifficulty(e))}'>سخت</button>
   </div>`;
 };
 
