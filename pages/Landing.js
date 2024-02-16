af0e9b99432ea4f7543f7d0c5f066bdeea261f5e
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2024-02-16 17:36:16 +0330

    Update: landing Page - 02
    Fonts - Some Styles and Introduction Text To Start

--- pages/Landing.js
+++ pages/Landing.js
@@ -1,6 +1,8 @@
 const Landing = () => {
   return `
   <div class="landingContainer">
+  <h2>به بازی QuizApp خوش اومدی </h2>
+  <h4>برای شروع، میزان سختی مدنظرت رو انتخاب کن تا بازی شروع شه</h4>
     <button>ساده</button>
     <button>متوسط</button>
     <button>سخت</button>
