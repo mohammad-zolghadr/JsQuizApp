02fa09d004770f98266e2e09fd69e4314fbb79ce
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2024-02-18 18:06:01 +0330

    Update: Game Page - GameQuestion
    UI of Question and Answer Section is Completed

--- pages/Game.js
+++ pages/Game.js
@@ -11,32 +11,54 @@ const Game = (mode) => {
   };
   return `
   <div class="gameContainer">
-  <header class="gameHeaderContainer">
-    <span>سطح ${faMode()}</span>
-    <div class="timerWrapper">
-    <span>00:59</span>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-        <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-13a.75.75 0 0 0-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 0 0 0-1.5h-3.25V5Z" clip-rule="evenodd" />
-      </svg>
-    </div>
-  </header>
+    <header class="gameHeaderContainer">
+      <span>سطح ${faMode()}</span>
+      <div class="timerWrapper">
+      <span>00:59</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-13a.75.75 0 0 0-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 0 0 0-1.5h-3.25V5Z" clip-rule="evenodd" />
+        </svg>
+      </div>
+    </header>
   
-  <div class="gameBodyContainer">
-    <div class="gameResultContainer">
-      <div class="gameResultCorrectContainer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-          <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
-        </svg>
-        <span>5</span>
+    <div class="gameBodyContainer">
+      <div class="gameResultContainer">
+        <div class="gameResultCorrectContainer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+          </svg>
+          <span>5</span>
+        </div>
+        <div class="gameResultWrongContainer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+          </svg>
+          <span>4</span>
+        </div>
       </div>
-      <div class="gameResultWrongContainer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
-        </svg>
-        <span>4</span>
+
+      <div class="gameQAContainer">
+        <p class>متن سوال</p>
+        <div class="gameAnswerContainer">
+          <div class="gameEachAnswerContainer">
+            <span></span>
+            <p>گزینه 1</p>
+          </div>
+          <div class="gameEachAnswerContainer">
+            <span></span>
+            <p>گزینه 2</p>
+          </div>
+          <div class="gameEachAnswerContainer">
+            <span></span>
+            <p>گزینه 3</p>
+          </div>
+          <div class="gameEachAnswerContainer">
+            <span></span>
+            <p>گزینه 4</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </div>`;
 };
 
