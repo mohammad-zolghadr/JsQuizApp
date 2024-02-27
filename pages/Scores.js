81515aea96a65c0060237cc273e60a4ef572181f
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2024-02-27 18:36:36 +0330

    Create: SCORE_PAGES
    some html is completed

--- pages/Scores.js
+++ pages/Scores.js
@@ -1,5 +1,22 @@
 const Scores = () => {
-  return `<h2>Scores Page</h2>`;
+  return `
+    <div class="scoreContainer">
+      <span class="currentScore">16</span>
+      <p class="bestScore">بهترین امتیاز: ${'45'}</p>
+      <div class="scoreBtnsContainer">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>     
+        </div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  `;
 };
 
 export default Scores;
